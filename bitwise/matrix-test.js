7a424504438d2a5e276e0823a95228070131d0c2
Sakari.Ruoho <finnishandy@gmail.com>
2016-04-22 17:17:42 +0100

    more functions

--- bitwise/matrix-test.js
+++ bitwise/matrix-test.js
@@ -1,22 +1,40 @@
 /**
  * Created by Finnishandy on 25/01/2016.
  */
-
+https://msdn.microsoft.com/en-us/library/t7f48wx9(v=vs.94).aspx
 //plane = factor ^ 2
 
-var t = 10000  + 3 + 6;
 
-var z = t >> 16, y = t >> 8, x = parseInt(((256 * 256 - 1) & t) / 256);
 
-console.log('x: ' + x);
-console.log('y: ' + y);
-console.log('z: ' + z);
+var maxnumber = 2147483647;
+
+var mask = 2147483647;
+
+var t = 255;
+
+console.log((8 + 32).toString(2));
+console.log((32).toString(2));
+console.log('res: ' + (mask ^ t).toString(2))
+console.log('2nd res: ' + ((mask ^ t) >> 2).toString(2))
+var foo = {z : 4, y : 6, x : 7};
+
+var num = 0;
+Object.keys(foo).forEach(function(key, ix) {
+    num += parseInt((Math.pow(8, ix + 1) + key));
+});
+console.log(num);
+console.log(num - (Math.pow(8, 3) + Math.pow(8, 2)));
+//console.log('x: ' + x);
+//console.log('y: ' + y);
+//console.log('z: ' + z);
 //console.log((t >> 16).toString(2));
 //console.log((t >> 8).toString(2));
-console.log((t >>> 3).toString(2));
+console.log((mask).toString(2));
 console.log((t).toString(2));
 //console.log(((256 * 256 - 1) & t) / 256);
 var fii = parseInt((t ).toString(2).substring(8, t.length ), 2) / 256;
 console.log(t);
 console.log(parseInt((t ).toString(2).substring(8, t.length ), 2));
 console.log(parseInt((t ).toString(2).substring(8, t.length ), 2));
+
+console.log(14 << 2);
